5ee82bdf8c8d3aa24f6562f3b5877e041c394287
Gerry <gerry.waterston@xdesign.com>
2024-08-30 14:56:10 +0100

    Completes lesson 6 to explore the framework.

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -18,4 +18,27 @@ test('homepage has title and links to intro page', async ({ page }) => {
   
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
+
+  await page.pause();
+});
+
+test('2:', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  // create a locator
+  const getStarted = page.getByRole('link', { name: 'Get started' });
+
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+
+  // Click the get started link.
+  await getStarted.click();
+  
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+
+  await page.pause();
 });
