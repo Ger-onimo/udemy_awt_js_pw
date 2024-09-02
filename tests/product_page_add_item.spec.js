0019a04b1ce521e70039653123197db1ff1d64ea
Gerry <gerry.waterston@xdesign.com>
2024-09-02 15:53:52 +0100

    Completes lesson 9 to assert changes

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -1,12 +1,19 @@
-import { test } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 
 test("Product Page Add to Basket", async ({ page }) => {
   await page.goto("localhost:2221")
 
-  const addToBasketButton = page.getByRole('button', { name: 'Add to Basket'}).first()
+  const addToBasketButton = page.locator('[data-qa="product-button"]').first()
+  const basketCounter = page.locator('[data-qa="header-basket-count"]')
+
   await addToBasketButton.waitFor()
+  await expect(addToBasketButton).toHaveText("Add to Basket")
+  await expect(basketCounter).toHaveText("0")
+
   await addToBasketButton.click()
   
-  await page.pause()
+  await expect(addToBasketButton).toHaveText("Remove from Basket")
+  await expect(basketCounter).toHaveText("1")
+  // await page.pause()
 
 })
