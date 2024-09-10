d654f378dcdff4824855517076a414470509a311
Gerry <gerry.waterston@xdesign.com>
2024-09-10 11:40:15 +0100

    Changes email/passwordElement to email/passwordInput. Add implements the UUID.

--- page-objects/RegisterPage.js
+++ page-objects/RegisterPage.js
@@ -1,20 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class RegisterPage {
   constructor(page) {
     this.page = page
 
-    this.emailElement = page.getByPlaceholder('E-Mail')
-    this.passwordElement = page.getByPlaceholder('Password')
+    this.emailInput = page.getByPlaceholder('E-Mail')
+    this.passwordInput = page.getByPlaceholder('Password')
     this.registerButton = page.getByRole('button', { name: 'Register' })
 
   }
 
   signUpAsNewUser = async () => {
    //type into email input
-   await this.emailElement.waitFor()
-   await this.emailElement.fill("qa@qa.com")
+   await this.emailInput.waitFor()
+   const emailId = uuidv4()
+   const email = emailId + "@qa.com"
+   await this.emailInput.fill(email)
   //  type into password input 
-  await this.passwordElement.waitFor()
-  await this.passwordElement.fill("pasword123")
+  await this.passwordInput.waitFor()
+  const password = uuidv4()
+  await this.passwordInput.fill(password)
   // click register button
   await this.registerButton.waitFor()
   await this.registerButton.click()
