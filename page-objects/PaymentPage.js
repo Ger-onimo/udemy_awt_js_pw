c898715e741330b977e3ef6e73625264d95ac25c
Gerry <gerry.waterston@xdesign.com>
2024-09-11 16:22:10 +0100

    Completes lesson 33 checking and comparing discounted value and message.

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -11,7 +11,11 @@ export class PaymentPage {
         this.discountInput = page.getByPlaceholder('Discount code')
         // this option works, not sure of reasoning to use the picker option as suggested above
         // this.discountInput = page.locator('[data-qa="discount-code-input"]')
-    }
+        this.activateDiscountButton = page.locator('[data-qa="submit-discount-button"]')
+        this.discountActivatedMessage = page.locator('[data-qa="discount-active-message"]')
+        this.totalValue = page.locator('[data-qa="total-value"]')
+        this.discountedValue = page.locator('[data-qa="total-with-discount-value"]')
+      }
 
     activateDiscount = async () => {
       await this.discountCode.waitFor()
@@ -20,13 +24,32 @@ export class PaymentPage {
       // Option 1 for laggy/slow inputs: using .fill() with await expect()
       await this.discountInput.fill(code)
       await expect(this.discountInput).toHaveValue(code)
-
       // Option 2 for laggy/slow inputs: slow typing. 
       // await this.discountInput.focus()
       // await this.page.keyboard.type(code, { delay: 500 })
       // expect (await this.discountInput.inputValue()).toBe(code)
-      // await this.page.pause() 
+      expect (await this.discountedValue.isVisible()).toBe(false)
+      expect (await this.discountActivatedMessage.isVisible()).toBe(false)
+      await this.activateDiscountButton.waitFor()
+      await this.activateDiscountButton.click()
+       // "Discount activated" message displays
+      await this.discountActivatedMessage.waitFor()
+      await expect(this.discountActivatedMessage)
+            .toHaveText("Discount activated!")
+      // check discounted total is showing
+      await this.discountedValue.waitFor()
+      const discountValueText = await this.discountedValue.innerText()
+      //  Strip out the $ and then parse the string to a number
+      const discountValueOnlyStringNumber = discountValueText.replace("$", "")
+      const discountValueNumber = parseInt(discountValueOnlyStringNumber, 10)
+      // check total is showing
+      await this.totalValue.waitFor()
+      const totalValueText = await this.totalValue.innerText()
+      const totalValueOnlyStringNumber = totalValueText.replace("$", "")
+      const totalValueNumber = parseInt(totalValueOnlyStringNumber, 10)
 
+      expect (discountValueNumber).toBeLessThan(totalValueNumber)
       
+      // await this.page.pause() 
     }
 }
